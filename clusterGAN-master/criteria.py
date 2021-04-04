25581e58f4e06b0d947761857a03502dee98d07a
bobliyilun <68295372+bobliyilun@users.noreply.github.com>
2021-04-04 04:04:10 -0400

    Add files via upload
    Metrics Implemented

--- clusterGAN-master/criteria.py
+++ clusterGAN-master/criteria.py
@@ -6,7 +6,6 @@ try:
     import numpy as np
 
     import matplotlib
-    matplotlib.use('Agg')
     import matplotlib.pyplot as plt
 
     import pandas as pd
@@ -37,44 +36,81 @@ except ImportError as e:
     print(e)
     raise ImportError
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+def main():
 
-classifier = Net().to(device)                 
-classifier.load_state_dict(torch.load("mnist_cnn.pt"))
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    cuda = True if torch.cuda.is_available() else False
 
-generator = Generator_CNN(30, 10, (1, 28, 28)).to(device) 
-encoder = Encoder_CNN(30, 10).to(device) 
+    classifier = Net().to(device)                 
+    generator = Generator_CNN(30, 10, (1, 28, 28)).to(device) 
+    encoder = Encoder_CNN(30, 10).to(device) 
 
-encoder_dict = torch.load("/home/srinath/Project/CSC413_Project/clusterGAN-master/runs/mnist/300epoch_z30_van_bs64_beta_c_12/models/encoder.pth.tar",
-                            map_location=device)  
-generator_dict = torch.load("/home/srinath/Project/CSC413_Project/clusterGAN-master/runs/mnist/300epoch_z30_van_bs64_beta_c_12/models/generator.pth.tar",
-                            map_location=device) 
+    encoder_dict = torch.load("C:\\Users\\BOBLY\\zzz\\final_project\\CSC413_Project\\clusterGAN-master\\runs\\mnist\\900epoch_z30_van_bs64_test_run\\models\\encoder.pth.tar",
+                                map_location=device)  
+    generator_dict = torch.load("C:\\Users\\BOBLY\\zzz\\final_project\\CSC413_Project\\clusterGAN-master\\runs\\mnist\\900epoch_z30_van_bs64_test_run\\models\\generator.pth.tar",
+                                map_location=device) 
 
-generator.load_state_dict(generator_dict)
-encoder.load_state_dict(encoder_dict)
+    classifier.load_state_dict(torch.load("mnist_cnn.pt"))
+    generator.load_state_dict(generator_dict)
+    encoder.load_state_dict(encoder_dict)
 
-generator.eval()
-encoder.eval()
-classifier.eval()
+    if cuda:
+        classifier.cuda()
+        encoder.cuda()
+        generator.cuda()
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-# mode accuracy
-zn_samp, zc_samp, zc_samp_idx = sample_z(shape=1000, latent_dim=30, n_c=10, fix_class=0)
-gen_imgs_samp = generator(zn_samp, zc_samp)
-output = classifier(gen_imgs_samp)
-print(zn_samp.requires_grad)
-generated = torch.argmax(output, 1)
-print(generated)
-print(zc_samp_idx)
+    generator.eval()
+    encoder.eval()
+    classifier.eval()
 
-# reconstruction accuracy
-# testdata = get_dataloader(train_set=False)
-# test_imgs, test_labels = next(iter(testdata)).cpu()
-# test_imgs = Variable(test_imgs.type(Tensor))
-# t_imgs, t_label = test_imgs.data, test_labels
-# e_tzn, e_tzc, e_tzc_logits = encoder(t_imgs)
-# teg_imgs = generator(e_tzn, e_tzc)
-# teg.requires_grad = False
-# reconstruction_labels = torch.argmax(classifier(teg_imgs), 1).cpu()
-# print(test_labels - reconstruction_labels)
+    # mode accuracy
+    num_classes = 10
+    shape_constant = 1000
+    correct = 0
+    labels = {}
+    for i in range(num_classes):
+        zn_samp, zc_samp, zc_samp_idx = sample_z(shape=shape_constant, latent_dim=30, n_c=10, fix_class=i)
+        gen_imgs_samp = generator(zn_samp, zc_samp)
+        output = classifier(gen_imgs_samp)
+        generated = torch.argmax(output, 1)
+
+        majority = torch.bincount(generated)
+        label = torch.argmax(majority)
+        count = torch.max(majority)
+        correct += count
+        labels[i] = label
+
+    mode_accuracy = correct.item()/(num_classes * shape_constant)
+    print(f"mode accuracy: {mode_accuracy}\n")
+
+    # reconstruction accuracy
+    batch_size = 5000
+    testdata = get_dataloader(train_set=False, batch_size=batch_size)
+    test_imgs, test_labels = next(iter(testdata))
+    test_imgs = Variable(test_imgs.type(Tensor))
+
+    t_imgs = test_imgs.data
+    e_tzn, e_tzc, e_tzc_logits = encoder(t_imgs)
+    teg_imgs = generator(e_tzn, e_tzc)
+    reconstruction_labels = torch.argmax(classifier(teg_imgs), 1)
+    reconstruction_loss = torch.sum(torch.isclose(test_labels.cuda(), reconstruction_labels.cuda(), 0, 0, True)).item()/batch_size
+    print(f"reconstruction loss: {reconstruction_loss}\n")
+
+    # cluster accuracy
+    encoder_result = torch.argmax(e_tzc_logits, 1)
+    mapping = torch.LongTensor([labels[i] for i in encoder_result.tolist()])
+    ACC = torch.sum(torch.isclose(test_labels.cuda(), mapping.cuda(), 0, 0, True)).item()/batch_size
+    NMI = normalized_mutual_info_score(test_labels, mapping)
+    ARI = adjusted_rand_score(test_labels, mapping)
+
+    print(f"Cluster Accuracy: {ACC}\n")
+    print(f"NMI: {NMI}\n")
+    print(f"ARI: {ARI}\n")
+
+
+if __name__ == "__main__":
+    main()
+
 
 
