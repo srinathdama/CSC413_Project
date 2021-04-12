71e2d03ed48b24b4ad25446faa1dc4c05205fabc
bobliyilun <68295372+bobliyilun@users.noreply.github.com>
2021-04-12 05:18:01 -0400

    cifar-models

--- clusterGAN-master/clusgan/__init__.py
+++ clusterGAN-master/clusgan/__init__.py
@@ -4,6 +4,7 @@ from .__version__ import __version__
 
 from .utils import *
 from .models import *
+from .models_cifar import *
 from .plots import *
 from .datasets import *
 from .definitions import *
--- clusterGAN-master/clusgan/datasets.py
+++ clusterGAN-master/clusgan/datasets.py
@@ -54,7 +54,7 @@ def get_dataloader(dataset_name='mnist', data_dir='', batch_size=64, train_set=T
         dset("C:\\Users\\BOBLY\\zzz\\cifar-10", train=train_set, download=False,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
-                           #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
                        ])),
         num_workers=num_workers,
         batch_size=batch_size,
